7241ebfcdb8907dce69489b03cae5a6c60b6c564
Timo Kaipiainen <timo.kaipiainen@nordcloud.com>
2018-03-22 23:28:45 +0200

    column names updated

--- app.py
+++ app.py
@@ -548,8 +548,8 @@ class Settings(db.Model):
     """ORM object for settings data
     """
     id = db.Column(db.Integer, primary_key=True)
-    key = db.Column(db.String(80), nullable=True)
-    value = db.Column(db.String(80), nullable=True)
+    setting_key = db.Column(db.String(80), nullable=True)
+    setting_value = db.Column(db.String(80), nullable=True)
 
 
 class User(db.Model):
