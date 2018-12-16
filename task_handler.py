3d982ddaca5e146672b9027a27a5a35ea456e346
Timo Kaipiainen <timo.kaipiainen@nordcloud.com>
2018-12-16 21:59:57 +0200

    autopep8

--- task_handler.py
+++ task_handler.py
@@ -9,37 +9,40 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 mail = Mail(app)
 
+
 def handleEmail(task):
     with app.app_context():
-       msg = Message("Imoittautumisesi JYPS Ry:n tapahtumaan",
-                     sender="noreply@jyps.fi",
-                    recipients=[task.target],
-                     body=task.param)
-       mail.send(msg)
-       task.status = 2
-       task.handled = datetime.datetime.now()
-       db.session.commit()
-       return
+        msg = Message("Imoittautumisesi JYPS Ry:n tapahtumaan",
+                      sender="noreply@jyps.fi",
+                      recipients=[task.target],
+                      body=task.param)
+        mail.send(msg)
+        task.status = 2
+        task.handled = datetime.datetime.now()
+        db.session.commit()
+        return
+
 
 def handleUserEmail(task):
     with app.app_context():
-       msg = Message("Tunnuksesi Jyps Ry:n tapahtumajärjestelmään",
-                     sender="noreply@jyps.fi",
-                     recipients=[task.target],
-                     body=task.param)
-       mail.send(msg)
-       task.status = 2
-       task.handled = datetime.datetime.now()
-       db.session.commit()
+        msg = Message("Tunnuksesi Jyps Ry:n tapahtumajärjestelmään",
+                      sender="noreply@jyps.fi",
+                      recipients=[task.target],
+                      body=task.param)
+        mail.send(msg)
+        task.status = 2
+        task.handled = datetime.datetime.now()
+        db.session.commit()
     return
 # mainlooop for taskhandler
 
+
 tasks = Task.query.filter_by(
-status=0).all()
+    status=0).all()
 
 for task in tasks:
-   if task.type == 1:
-      handleEmail(task)
-   if task.type == 2:
-      handleUserEmail(task)
-   db.session.flush()
+    if task.type == 1:
+        handleEmail(task)
+    if task.type == 2:
+        handleUserEmail(task)
+    db.session.flush()
