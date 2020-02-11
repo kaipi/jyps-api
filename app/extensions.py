fd9911e8e3b3a61a22161a12beabb3e1c4d004f8
Timo Kaipiainen <Ext-Timo.Kaipiainen@3stepit.com>
2020-02-11 22:09:56 +0200

    fixed cors

--- app/extensions.py
+++ app/extensions.py
@@ -5,7 +5,7 @@ from .event.models import *
 
 from flask_cors import CORS
 
-cors = CORS(supports_credentials=True)
+cors = CORS(resources={r"*": {"origins": "*"}})
 
 from flask_jwt_simple import JWTManager, jwt_required, create_jwt, get_jwt_identity
 
